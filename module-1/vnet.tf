caaba2e5c3d267488ade34f4701002bef0267331
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-09 11:05:18 +0100

    first testrun8

--- module-1/vnet.tf
+++ module-1/vnet.tf
@@ -7,30 +7,20 @@ terraform {
   }
 }
 
+# Create VNet
 resource "azurerm_virtual_network" "vnet" {
-  name = "vnet"
-  location = var.location
+  name                = "vnet"
+  location            = var.location
   resource_group_name = var.RGName
-  address_space = [ "10.0.0.0/16" ]
-  
+  address_space       = ["10.0.0.0/16"]
+
 }
 
-resource "azurerm_subnet" "subnet1" {
-  name = "subnet1"
-  resource_group_name = var.RGName
-   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = [ "10.0.1.0/24" ]
-    
-}
-
-resource "azurerm_subnet" "subnet2" {
-  name = "subnet2"
-  resource_group_name = var.RGName
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = var.RGName
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = [ "10.0.2.0/24" ]
-    
-}
+  address_prefixes     = ["10.0.10.0/24"]
 
-output "azurerm_subnet_id" {
-  value = azurerm_subnet.subnet.id
 }
