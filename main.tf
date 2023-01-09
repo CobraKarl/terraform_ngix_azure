c24299452c6d599bcefe18c78d74759d65f8e1fb
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-09 10:43:59 +0100

    first testrun

--- main.tf
+++ main.tf
@@ -13,3 +13,66 @@ provider "azurerm" {
       
     } 
 }
+
+# Create RG
+resource "azurerm_resource_group" "rg" {
+  name     = var.RGName
+  location = var.location
+
+}
+
+module "vnet" {
+    source = "vnet.tf"
+    depends_on = [
+      azurerm_resource_group.rg
+    ]
+  
+}
+
+# Create NSG
+resource "azurerm_network_security_group" "allowedports" {
+    name = "allowedports"
+    resource_group_name = var.RGName
+    location = var.location
+
+    security_rule = {
+        name: "http"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "TCP"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    
+    security_rule = {
+        
+        name = "https"
+        priority = 200
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "443"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    
+    }
+
+    security_rule = {
+        name = "ssh"
+        priority = 300
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+
+    }
+    
+    } 
+}
+
---
+++ vnet.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name = "vnet"
+    location = var.location
+    resource_group_name = var.RGName
+    address_space = [ "10.0.0.0/16" ]
+  
+}
+
+resource "azurerm_subnet" "subnet1" {
+    name = "subnet1"
+    resource_group_name = var.RGName
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = [ "10.0.1.0/24" ]
+    
+}
+
+resource "azurerm_subnet" "subnet2" {
+    name = "subnet2"
+    resource_group_name = var.RGName
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = [ "10.0.2.0/24" ]
+    
+}
