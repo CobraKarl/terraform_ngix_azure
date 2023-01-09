33323fb7e6616e548c2821e8be7efa1e4f33198f
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-09 11:13:48 +0100

    first testrun10

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = var.subscriptionId
-    client_id = var.clientId
-    client_secret = var.clientSecret
-    tenant_id = var.tenantId
-    features {
-      
-    } 
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
+  features {
+
+  }
 }
 
 # Create RG
@@ -25,72 +25,76 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "vnet"
-  location = var.location
+  name                = "vnet"
+  location            = var.location
   resource_group_name = var.RGName
-  address_space = [ "10.0.0.0/16" ]
-  
+  address_space       = ["10.0.0.0/16"]
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name = "subnet1"
-  resource_group_name = var.RGName
-   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = [ "10.0.1.0/24" ]
-    
+  name                 = "subnet1"
+  resource_group_name  = var.RGName
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name = "subnet2"
-  resource_group_name = var.RGName
+  name                 = "subnet2"
+  resource_group_name  = var.RGName
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = [ "10.0.2.0/24" ]
-    
+  address_prefixes     = ["10.0.2.0/24"]
+
 }
 
 # Create NSG
 resource "azurerm_network_security_group" "allowedports" {
-   name = "allowedports"
-   resource_group_name = var.RGName
-   location =var.location
-  
-   security_rule {
+  name                = "allowedports"
+  resource_group_name = var.RGName
+  location            = var.location
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 
-
-    name = "http"
-    priority = 100
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "80"
-    source_address_prefix = "*"
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
-   }
+  }
 
-   security_rule {
+  security_rule {
 
-    name = "https"
-    priority = 200
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "443"
-    source_address_prefix = "*"
+    name                       = "https"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
-   }
+  }
 
-   security_rule {
-    name = "ssh"
-    priority = 300
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "22"
-    source_address_prefix = "*"
+  security_rule {
+    name                       = "ssh"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
-   }
+  }
 }
 
