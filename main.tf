2ea9f545720b6266d7fa61294d3faa2f2c48fe1d
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-09 10:15:51 +0100

    added var & main

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = "->2.91.0"
+  }
+}
+
+provider "azurerm" {
+    subscription_id = var.subscriptionId
+    client_id = var.clientId
+    client_secret = var.clientSecret
+    tenant_id = var.tenantId
+    features {
+      
+    } 
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "RGName" {
+  type = string
+
+}
+
+variable "location" {
+  type = string
+
+}
+
+variable "subscriptionId" {
+  type = string
+
+}
+
+variable "clientId" {
+  type = string
+}
+
+variable "clientSecret" {
+  type = string
+}
+
+variable "tenantId" {
+  type = string
+
+}
